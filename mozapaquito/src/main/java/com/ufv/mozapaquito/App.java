57742c189969bb3a48f8e5593fe0ce07e7a17ef4
miguel llorens <miguel.llorenscarro@gmail.con>
2020-04-28 18:48:08 +0200

    Añadido leerJson

--- mozapaquito/src/main/java/com/ufv/mozapaquito/App.java
+++ mozapaquito/src/main/java/com/ufv/mozapaquito/App.java
@@ -1,5 +1,6 @@
 package com.ufv.mozapaquito;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -10,6 +11,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -27,29 +30,33 @@ public class App
         System.out.println( "Hello World!" );
         
         ArrayList<Contacto> contactos = new ArrayList<>();
-        leerJason(contactos);
-        anadirContacto("caca", contactos);
-        anadirContacto("pepe", contactos);
+       
+        anadirContacto("cree", contactos);
+        anadirContacto("vvc", contactos);
         try (Writer writer = new FileWriter("Output.json")) {
             Gson gson = new GsonBuilder().create();
             gson.toJson(contactos, writer);
         } catch (IOException e) {}
-        
+      leerJason(contactos);
     }
     public static void anadirContacto(String name, ArrayList<Contacto> contacts) {
     	Contacto contacto = new Contacto(name);
     	contacts.add(contacto);
     }
     public static void leerJason(ArrayList<Contacto> contacts) {
+    	Gson gson = new Gson();
+    	Type userListType = new TypeToken<ArrayList<Contacto>>(){}.getType();
     	try {
-    	    // create Gson instance
-    	    Gson gson = new Gson();
-    	    // create a reader
-    	    Reader reader = Files.newBufferedReader(Paths.get("Output.json"));
-    	    // convert JSON string to User object
-    	    contacts.add(gson.fromJson(reader, Contacto.class));
-    	    reader.close();
-
+    	    File	archivo  = new File("alumnos.json");
+			FileReader fr = new FileReader(archivo);
+			
+			//BufferedReader br = new BufferedReader(fr);
+			
+			
+			contacts = gson.fromJson(fr, userListType);
+			
+		   //String linea = br.readLine();
+		   System.out.println(contacts.get(0));
     	} catch (Exception ex) {
     	    ex.printStackTrace();
     	}
